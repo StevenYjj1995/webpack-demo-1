729d68a3c56e0ebec87ef355d9a8e1b79466e6bd
steven <steven.yjj1995@gmail.com>
2021-03-24 20:13:09 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    path: __dirname + "/dist",
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "叶健杰",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
